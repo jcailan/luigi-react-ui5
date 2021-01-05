579d6fe4a1115ece3e23a85230d32f47ef8a7320
Jhodel Cailan <jhodel.cailan@gmail.com>
2021-01-05 17:07:06 +0800

    Step 8: Create "Home" view

---
+++ src/views/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { LayoutPanel } from 'fundamental-react';
+
+export const Home = () => {
+	return (
+		<LayoutPanel>
+			<LayoutPanel.Body>
+				<h2>Welcome to Luigi - a micro-frontend framework</h2>
+			</LayoutPanel.Body>
+		</LayoutPanel>
+	);
+};
