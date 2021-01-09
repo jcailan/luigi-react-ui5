2a83f4e8e7442d8cdf8638847de82eca3660050c
Jhodel Cailan <jhodel.cailan@gmail.com>
2021-01-09 11:01:05 +0800

    Step 4: Add language selection dropdown to the homepage

--- react-core-mf/src/views/Home.jsx
+++ react-core-mf/src/views/Home.jsx
@@ -1,11 +1,28 @@
-import React from 'react';
-import { LayoutPanel } from 'fundamental-react';
+import React, { useState } from 'react';
+import { LayoutPanel, Select } from 'fundamental-react';
+import LuigiClient from '@luigi-project/client';
 
-export const Home = () => {
+export const Home = ({ localeDict, currentLocale }) => {
+	const [locale, setLocale] = useState(currentLocale);
+	const options = [{ key: 'en-US', text: 'en-US' }, { key: 'de-DE', text: 'de-DE' }];
+
+	const onChangeValue = (event) => {
+		setLocale(event.target.textContent);
+		LuigiClient.sendCustomMessage({ id: 'set-language', 'locale': event.target.textContent });
+	};
+
 	return (
 		<LayoutPanel>
 			<LayoutPanel.Body>
-				<h2>Welcome to Luigi - a micro-frontend framework</h2>
+				<h2>{localeDict.WELCOME_LUIGI}</h2>
+				<br />
+				<span style={{ marginRight: "12px" }}>{localeDict.SELECT_LANGUAGE}</span>
+				<Select
+					options={options}
+					placeholder={localeDict.SELECT_LANGUAGE}
+					onSelect={(e) => onChangeValue(e)}
+					selectedKey={locale}
+				/>
 			</LayoutPanel.Body>
 		</LayoutPanel>
 	);
